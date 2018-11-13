f936a035a638b832b1cba28d0ab523f132618042
Drabenstein <marcindrabek@hotmail.com>
2018-11-13 14:24:17 +0100

    Added CStringHelper

---
+++ Lista3/Lista3/CStringHelper.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CStringHelper.h"
+
+bool CStringHelper::bStartsWith(std::string sText, std::string sToken)
+{
+	if(sText.length() < sToken.length())
+	{
+		return false;
+	}
+
+	for(int i = 0; i < sToken.length(); i++)
+	{
+		if(sText[i] != sToken[i])
+		{
+			return false;
+		}
+	}
+
+	return true;
+} // bool CStringHelper::bStartsWith(std::string sToken)
---
+++ Lista3/Lista3/CStringHelper.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+class CStringHelper
+{
+public:
+	bool bStartsWith(std::string sText, std::string sToken);
+};
